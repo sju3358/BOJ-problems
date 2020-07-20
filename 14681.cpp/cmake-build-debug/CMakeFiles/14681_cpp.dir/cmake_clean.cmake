1e38f97ece9ecaf904c2a3630a9283702ec20828
sju3358 <sju3358@hanmail.net>
2020-07-20 21:20:06 +0900

    사분면 문제

---
+++ 14681.cpp/cmake-build-debug/CMakeFiles/14681_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "14681_cpp.exe"
+  "14681_cpp.exe.manifest"
+  "14681_cpp.pdb"
+  "CMakeFiles/14681_cpp.dir/main.cpp.obj"
+  "lib14681_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/14681_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
