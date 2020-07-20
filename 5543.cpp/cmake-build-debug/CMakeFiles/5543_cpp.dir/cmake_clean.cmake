604041b56a9d0e2afa52d8855ef832012d4caee0
sju3358 <sju3358@hanmail.net>
2020-07-20 21:30:33 +0900

    햄버거 문제

---
+++ 5543.cpp/cmake-build-debug/CMakeFiles/5543_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5543_cpp.exe"
+  "5543_cpp.exe.manifest"
+  "5543_cpp.pdb"
+  "CMakeFiles/5543_cpp.dir/main.cpp.obj"
+  "lib5543_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5543_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 5543.cpp/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int hamburg;
+    int beverage;
+    int min;
+    int sum = 0;
+
+    cin >> hamburg;
+    min = hamburg;
+
+    cin >> hamburg;
+    if (min > hamburg)
+        min = hamburg;
+
+    cin >> hamburg;
+    if(min > hamburg)
+        min = hamburg;
+
+    sum = min;
+
+    cin >> beverage;
+    min = beverage;
+
+    cin >> beverage;
+    if(min > beverage)
+        min = beverage;
+
+    sum += min;
+
+    sum -= 50;
+
+    cout <<sum<<endl;
+
+}
