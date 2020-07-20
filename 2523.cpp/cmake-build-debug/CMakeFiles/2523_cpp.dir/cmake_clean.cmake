0c05fc79503246320ea4bba89eca5530c95c5776
sju3358 <sju3358@hanmail.net>
2020-07-20 21:44:18 +0900

    .

---
+++ 2523.cpp/cmake-build-debug/CMakeFiles/2523_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2523_cpp.exe"
+  "2523_cpp.exe.manifest"
+  "2523_cpp.pdb"
+  "CMakeFiles/2523_cpp.dir/main.cpp.obj"
+  "lib2523_cpp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2523_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
